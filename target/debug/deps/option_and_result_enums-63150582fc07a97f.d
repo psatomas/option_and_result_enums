5b894f6ab2e19c5cde1c63cb1d98be6e19abfa3b
psatomas <psatomas@gmail.com>
2025-07-15 17:57:42 -0300

    first commit

---
+++ target/debug/deps/option_and_result_enums-63150582fc07a97f.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/option_and_result_enums/target/debug/deps/liboption_and_result_enums-63150582fc07a97f.rmeta: src/main.rs
+
+/home/tomas/Documents/projects/rust/option_and_result_enums/target/debug/deps/option_and_result_enums-63150582fc07a97f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/option_and_result_enums-f0670bff867fc76e.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/option_and_result_enums/target/debug/deps/liboption_and_result_enums-f0670bff867fc76e.rmeta: src/main.rs
+
+/home/tomas/Documents/projects/rust/option_and_result_enums/target/debug/deps/option_and_result_enums-f0670bff867fc76e.d: src/main.rs
+
+src/main.rs:
