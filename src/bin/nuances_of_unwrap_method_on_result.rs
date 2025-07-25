e550087bcb9ddfbdb8fedb1c0440b7497cbd3aa3
psatomas <psatomas@gmail.com>
2025-07-25 06:16:30 -0300

    feat: add nuances_of_unwrap_method_on_result.rs

---
+++ src/bin/nuances_of_unwrap_method_on_result.rs
@@ -0,0 +1,18 @@
+fn operation(great_success: bool) ->  Result<&'static str, &'static str> {
+    if great_success {
+        Ok("success")
+    } else {
+        Err("Error")
+    }
+}
+
+fn main() {
+    let my_result = operation( true);
+
+    let content = match my_result {
+        Ok(message) => message,
+        Err(error) => error,
+    };
+
+    println!("{}", my_result.unwrap());
+}
